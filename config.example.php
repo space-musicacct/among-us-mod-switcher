e0ebe06e8a4239685d2c696019c09d8c7eb49b27
space <yuma.dark.black@gmail.com>
2026-01-11 21:57:34 +0900

    ( )を全角 → 半角に変更

--- config.example.php
+++ config.example.php
@@ -5,7 +5,7 @@
 declare(strict_types=1);
 
 return [
-    // Steam の common フォルダ（Among Us が入ってる場所）
+    // Steam の common フォルダ (Among Us が入ってる場所)
     // 例: C:\Program Files (x86)\Steam\steamapps\common
     'steam_common_dir' => 'C:\\Program Files (x86)\\Steam\\steamapps\\common',
 
@@ -15,13 +15,13 @@ return [
     // id.yaml のファイル名
     'yaml_name' => 'id.yaml',
 
-    // Steam AppID（Among Us）
+    // Steam AppID (Among Us)
     'steam_app_id' => '945360',
 
-    // ログ（Among Us フォルダ配下に作る）
+    // ログ (Among Us フォルダ配下に作る)
     'log_relpath' => 'log\\switch.log',
 
     // appトークン
-    // 任意の長い文字列を入れてください（ランダム生成推奨）
+    // 任意の長い文字列を入れてください
     'app_token' => 'VfYJ2Qq8zzc8b1QAFVfcD858ardEhbrQZtuG9JmQfnLOH0zY6ZNBoTiH1br001Xo',
 ];
